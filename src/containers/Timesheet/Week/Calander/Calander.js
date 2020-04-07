0e9e5ebcd560830de9954f7602bd03bbe9d9c077
Fenting Lin <fen@pdx.edu>
2020-04-06 22:17:50 -0400

    reorgnize timesheet structure

--- src/containers/Timesheet/Week/Calander/Calander.js
+++ src/containers/Timesheet/Week/Calander/Calander.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Calander.css';
 class Calander extends Component { 
-      render() {
-        return (
-            <div class="form-group row" className="calander">
-                <input type="date" className="form-control" />
-            </div>
-        )
-      }
+  constructor(props) {
+    super(props);
+    const now = new Date().toISOString().split("T")[0];
+
+    this.state = {
+      date: props.calanderDate === undefined ? now : props.date
+    }
+  }
+
+  dateChange() {
+    // TODO
+  }
+
+  render() {
+    return (
+        <div class="form-group row" className="calander">
+            <input type="date" className="form-control" value={this.state.date} id="date"/>
+        </div>
+    )
+  }
 }
 
 export default Calander;
