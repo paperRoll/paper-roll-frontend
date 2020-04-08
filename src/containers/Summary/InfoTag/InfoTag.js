c81330527aa6743c955b4e6dca0a735af7fcb567
Cara <jiangyucara@gmail.com>
2020-04-07 23:13:18 -0400

    summary page part finished but no backend functions

---
+++ src/containers/Summary/InfoTag/InfoTag.js
@@ -0,0 +1,36 @@
+import React, { Component, useRef, useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function useHover() {
+    const ref = useRef();
+    const [hovered, setHovered] = useState(false)
+
+    const enter = () => setHovered(true)
+    const leave = () => setHovered(false)
+
+    useEffect(() => {
+        const referrence = ref.current
+        referrence.addEventListener('mouseenter', enter)
+        referrence.addEventListener('mouseleave', leave)
+        return () => {
+            referrence.removeEventListener('mouseenter', enter)
+            referrence.removeEventListener('mouseleave', leave)
+        }
+    }, [ref])   
+
+    return [ref, hovered]      
+}
+
+function InfoTag() {
+
+    const [ref, hovered] = useHover()
+
+    return (
+        <div>
+            <span  style={{fontFamily: "Italics"}}class="badge badge-pill badge-info" ref={ref}>i</span>
+            {hovered && <div>Hi!</div>}
+        </div>
+        
+    );
+}
+ export default InfoTag;
---
+++ src/containers/Summary/LoadMore/LoadMore.js
@@ -0,0 +1,15 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './LoadMore.css';
+
+
+function LoadMore() {
+    return (
+        <div>
+            <button className="loadmore">Load More</button>
+        </div>
+    );        
+    
+}
+
+export default LoadMore;
