eb48c26b4e8dd3e058b7f2c10f200b62f22c350f
Martin Li <lixphilosophy@gmail.com>
2020-04-08 18:40:37 -0400

    clean up proj

--- src/containers/Summary/InfoTag/InfoTag.js
+++ src/containers/Summary/InfoTag/InfoTag.js
@@ -1,4 +1,4 @@
-import React, { Component, useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function useHover() {
